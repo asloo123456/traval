92075fcb5953cca820f3f53ec63bfa26731b7c45
wangjingpeng1904 <1178491130@qq.com>
2019-09-18 20:48:10 +0800

    彭

--- traval/shutterbug/vue_app_000/vue_app_00/src/components/pat/career.vue
+++ traval/shutterbug/vue_app_000/vue_app_00/src/components/pat/career.vue
@@ -1,5 +1,33 @@
 <template>
     <div class="post_comme">
+        <h1 style="font-size:.9rem;color:#fff;text-align:center">工作机会</h1>
+        <h3 style="font-size:.5rem;color:#dc223c">高至影像科技</h3>
+        <h3 style="font:.4rem 'HanHei';color:#fff">工作地点</h3>
+        <p style="color:#a0a0a0;font:.3rem 'HanHei'">杭州</p>
+        <h3 style="font:.4rem 'HanHei';color:#fff;margin-top:.8rem">基本要求</h3>
+        <ul style="padding-left:.6rem; list-style:disc">
+          <li style="padding:0;font:0.27rem 'HeiTi';color:#DBDBDB;margin-bottom:.2rem ">热爱摄影， 热爱影像，热爱互联网，热爱与视觉相关的事物， 能够理解美食一种有逻辑综合感受， 而非单一的视觉刺激</li>
+          <li style="padding:0;font:0.27rem 'HeiTi';color:#DBDBDB;margin-bottom:.2rem  ">能够跟踪和学习最新的软件技术，对本专业的对接专业有足够了解</li>
+          <li style="padding:0;font:0.27rem 'HeiTi';color:#DBDBDB; ">有良好的自律和自我管理能力、创新情结和创业心态，善于沟通，乐于助人</li>
+        </ul>
+        <h3 style="font:.5rem 'HanHei';color:#fff">简历投递</h3>
+        <p style="color:#A0A0A0;font:.3rem 'HanHei'">e-Mail: yujg@syzychina.com</p>
+        <h4 style="color:#DC223C;font:.4rem 'HanHei'"><span>[职位]</span><span>JDNME_内容运营/编辑</span></h4>
+        <p style="color:#A0A0A0;font:.3rem 'HanHei'">
+          职位描述:<br>
+          1. 独立寻找选题并制作，完成日常新媒体稿件的采编;<br>
+          2. 包含微信、微博在内的多个内容平台的日常运营;<br>
+          3. 定期策划H5、线上活动或其它互联网产品，以达到提高知名度、活跃用户、转化粉丝、商业变现等目的。
+        </p>
+        <p style="color:#A0A0A0;font:.3rem 'HanHei'">
+          任职要求:<br>
+          1. 热爱摄影的高普青年。<br>
+          2. 对摄影的理论、器材、文化或技巧中至少一个领域有较多了解。了解摄影爱好者群体，有较高审美能力。<br>
+          3. 能够熟练掌握office办公软件，能熟练使用Adobe Photoshop / illustrator / Premiere 等图片和视频编辑软件，擅长平面设计、手绘或网页设计者优先录用。<br>
+          4. 有内容电商运营经验者优先考虑。<br>
+          5. 思维活跃，吃苦耐劳，能摄影不定期需要脑暴的互联网工作节奏。
+        </p>
+        <!--评论-->
         <ul>
           <h2>评论:</h2>
            <li v-for="(task,i) of tasks"  :key="i">
@@ -61,7 +89,7 @@ export default {
        padding:.4rem .7rem;
        background:#1e1e1e;
     
-       h2{font-size:.2rem;color:#fff;margin:0 0 .5rem 0;}
+       h2{font-size:.3rem;color:#fff;margin:0 0 .5rem 0;}
        p{font-size:.2rem;color:#fff;margin:.3rem 0}
        label{font-size:.2rem;color:#595959}
        .form_control{width:5rem;height:3rem;background:#232323;border:0;font-size:.3rem;margin-bottom:.1rem;color:#fff; }
--- traval/shutterbug/vue_app_000/vue_app_00/src/router.js
+++ traval/shutterbug/vue_app_000/vue_app_00/src/router.js
@@ -13,6 +13,7 @@ import footer from "./components/footer/footer.vue"
 // 引入中间部分
 import Indexss from "./components/pat/Indexss.vue"
 import career from "./components/pat/career.vue"
+// import sc_Check from "./components/pat/sc_Check.vue"
 
 Vue.use(Router)
 export default new Router({
@@ -24,6 +25,7 @@ export default new Router({
 // 中间
     {path:'/Indexss',component:Indexss},
     {path:'/career',component:career},
+    // {path:'/sc_Check',component:sc_Check},
     //底部
     {path:'/footer',component:footer}, 
   ]
