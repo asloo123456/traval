8c61f1a745b676a973a2b381516a9237a309fc12
jtacad <1017966708@qq.com>
2019-09-20 15:56:23 +0800

    top

--- traval/shutterbug/vue_app_000/vue_app_00/src/components/top/top.vue
+++ traval/shutterbug/vue_app_000/vue_app_00/src/components/top/top.vue
@@ -9,20 +9,35 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+  .top{
+    width: 100%;
+    padding-right: 20px;
+    padding-left: 20px;
+    margin-right: auto;
+    margin-left: auto;
+  }
   /*超大屏幕下iframe的高度*/
   @media (min-width: 1120px) {
-
+    .top{
+      max-width: 1080px;
+    }
   }
   /*大屏幕下iframe的高度*/ 
   @media (max-width: 1119px) and (min-width: 1020px) {
+    .top{
+      max-width: 1000px;
+    }
 
   }
   /*中屏幕下iframe的高度*/ 
   @media (max-width: 1019px) and (min-width: 760px) {
+    .top{
+      max-width: 720px;
+    }
 
   }
   /*小屏幕下iframe的高度*/ 
-  @media (min-width: 759px) {
+  @media (max-width: 759px) {
     
   }
 </style>
