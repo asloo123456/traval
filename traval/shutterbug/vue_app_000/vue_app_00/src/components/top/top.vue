843f062a7f4278fe0e04d5756ad1d6b36ab2a852
jtacad <1017966708@qq.com>
2019-09-21 14:54:37 +0800

    top

--- traval/shutterbug/vue_app_000/vue_app_00/src/components/top/top.vue
+++ traval/shutterbug/vue_app_000/vue_app_00/src/components/top/top.vue
@@ -1,10 +1,46 @@
 <template>
   <div class="top">
+    <!-- 大屏和超大屏 -->
     <div class="top1">
-      <div></div>
-      <div></div>
+      <!-- 第一层 -->
+      <div class="top1_center1">
+        <!-- 左边 -->
+        <div>
+          <a href="#" style="border-radius: 2px;background-color: #474747;color: #eee;padding: 6px 16px;">
+            <span>RSS</span>
+          </a>
+        </div>
+        <!-- 中间logo -->
+        <div>
+          <a href="#">
+            <img src="../../assets/new_fotomen_logo2.png" alt="">
+          </a>
+        </div>
+        <!-- 右边放大镜 -->
+        <div style="display: flex;flex-direction: column;justify-content: center;">
+          <a style="width:18px;height:27px;padding:7px 0;margin-left:14px;display: flex;flex-direction: column;justify-content: center;" href="#">
+            <img style="width:18PX;height:18PX;" src="../../assets/sou(1).png" alt="">
+          </a>
+        </div>
+      </div>
+      <!-- 第二层 -->
+      <div class="top1_center2">
+        <!-- 左边三条横杠 -->
+        <div style="display: flex;flex-direction: column;justify-content: center;">
+          <button class="nav-mobile-button">
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- 右边 -->
+          <div></div>
+        </div>
+      </div>
     </div>
-    <div class="top2" style="display: flex;justify-content: space-between;">
+    <!-- 中屏和小屏 -->
+    <div class="top2">
+      <!-- 左边三条横杠 -->
       <div style="display: flex;flex-direction: column;justify-content: center;">
         <button class="nav-mobile-button">
           <span class="sr-only"></span>
@@ -13,16 +49,17 @@
           <span class="icon-bar"></span>
         </button>
       </div>
+      <!-- fotomen -->
       <div style="display: flex;flex-direction: column;justify-content: center;">
         <a href="#" class="fotomen">FOTOMEN</a>
       </div>
+      <!-- 右边放大镜 -->
       <div style="display: flex;flex-direction: column;justify-content: center;">
         <a style="width:18px;height:27px;padding:7px 0;margin-left:14px;display: flex;flex-direction: column;justify-content: center;" href="#">
           <img style="width:18PX;height:18PX;" src="../../assets/sou(1).png" alt="">
         </a>
       </div>
     </div>
-
   </div>
 </template>
 <script>
@@ -31,6 +68,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+  .top{
+    background: #111111;
+    width: 100%;
+    display: block;
+    box-sizing: border-box;
+  }
+  // 大屏和超大屏
+  .top1{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .top1_center1,.top1_center2{
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #232323;
+  }
+  .top1_center1{
+    height: 120px;
+  }
+  .top1_center2{
+    height: 50px;
+  }
+  // 中屏和小屏
+  .top2{
+    height: 49px;
+    border-bottom: 1px solid #232323;
+    display: flex;
+    justify-content: space-between;
+  }
+  // 三条很杠
   span{
     margin: 0;
   }
@@ -38,21 +106,8 @@ export default {
     margin: 0;
     padding: 0;
   }
-  .fotomen{
-    font-size: 22px;
-    font-weight: 700;
-    font-style: normal;
-    letter-spacing: -.015em;
-    line-height: 1;
-    text-transform: none;
-    color: #dadada;
-  }
-  .fotomen:hover{
-    color: #ffffff;
-  }
   .nav-mobile-button {
     background-color: transparent;
-    background-image: none;
     border: 1px solid transparent;
     border-radius: 4px; 
   }
@@ -67,21 +122,18 @@ export default {
     background: #ffffff;
     border-radius: 1px; 
   }
-  template{
-    font-size: 0px;
+  // fotomen
+  .fotomen{
+    font-size: 22px;
+    font-weight: 700;
+    font-style: normal;
+    letter-spacing: -.015em;
+    line-height: 1;
+    text-transform: none;
+    color: #dadada;
   }
-  .top{
-    background: #111111;
-    width: 100%;
-    display: block;
-    box-sizing: border-box;
-  }
-  .top1{
-    border-bottom: 1px solid #232323;
-  }
-  .top2{
-    height: 49px;
-    border-bottom: 1px solid #232323;
+  .fotomen:hover{
+    color: #ffffff;
   }
   /*超大屏幕下iframe的高度*/
   @media (min-width: 1120px) {
